82c029fdfe05bfcbe91732e00b4ccb89e14c7c7d
longfei.fu <longfei.fu@thermofisher.com>
2022-06-22 19:14:46 +0800

    update

--- pileup2vaf.py
+++ pileup2vaf.py
@@ -16,6 +16,42 @@ def parse_args():
         AP.add_argument('-od',help="output dir",dest="outdir")
         return AP.parse_args()
 
+BASE = ['A','T','C','G']
+
+def parse_indel(s):
+    '''
+    s: string
+    C+4TACT
+    A-1G
+    
+    return value(ref,alt)
+    '''
+    val = []
+    if '+' in s:
+        # Ins
+        # 'C+4TACT' => VCF record is: chr pos C CTACT
+        alt_allele = []
+        for i in s:
+            if i in BASE:
+                alt_allele.append(i.upper())
+
+        ref = alt_allele[0] 
+        alt = "".join(alt_allele)
+    else:
+        # Del
+        # 'G-1C' => VCF record is: chr pos GC G
+        alt_allele = []
+        for i in s:
+            if i in BASE:
+                alt_allele.append(i.upper())
+        
+        ref = "".join(alt_allele) # GC
+        alt = ref[0]              # G
+
+    val.append(ref)
+    val.append(alt)
+    return(val)
+
 def parse_query(chrom,pos,ref,query):
     '''
     1.count depth
@@ -23,13 +59,8 @@ def parse_query(chrom,pos,ref,query):
     3.ins
     4.del
     5.cal freq
-    '''
-
-    '''
+  
     query format
-
-    HIV-1 1640 G 16 ['G', 'G', 'G', 'g', 'g', 'g', 'g', 'G', 'G', 'G', 'g', 'g', 'G', 'G', 'g', 'G']
-    {'G': 9, 'g': 7}
     HIV-1 1641 A 16 ['A', 'A', 'A', 'a', 'a', 'g', 'a', 'A', 'A', 'A', 'a', 'g', 'A', 'A', 'a', 'A']
     {'A': 9, 'a': 5, 'g': 2}
     HIV-1 1642 A 16 ['G', 'G', 'G', 'g', 'g', 'a', 'g', 'A-1G', 'G', 'A', 'a', 'a', 'G', 'G', 'g', 'G']
@@ -40,23 +71,14 @@ def parse_query(chrom,pos,ref,query):
     {'A': 8, 'a': 6, 'a+2at': 1}
     HIV-1 1645 T 15 ['T', 'T', 'T', 't', 't', 't', 't', 'T', 'T', 't', 't', 'T', 'T', 't', 'T']
     {'T': 8, 't': 7}
-    HIV-1 1646 C 15 ['T', 'T', 'T', 't', 't', 't', 't', 'T', 'T', 't', 't', 'T', 'T', 't', 'T']
-    {'T': 8, 't': 7}
-    HIV-1 1647 T 15 ['T', 'T', 'T', 't', 't', 't', 't', 'T', 'T', 't', 't', 'T', 'T', 't', 'T']
-    {'T': 8, 't': 7}
-    HIV-1 1648 G 16 ['G', 'G', 'G', 'g', 'g', 'g', 'g', 'G', 'G', 'g', 'g', 'G', 'G', 'g', 'G', 'g']
-    {'G': 8, 'g': 8}
 
     '''
     BASE = ['A','T','C','G']
-    #print(chrom,pos,ref)
 
     dict_all = defaultdict(lambda:0)
     for item in query:
         '''
-        {'A': 9, 'a': 5, 'g': 2} => {'A':14, 'G':2}
-        func upper() will trans 'g+1a' into 'G+1A'
-
+        {'A': 9, 'a': 5, 'g': 2}     => {'A':14, 'G':2}
         {'A': 8, 'a': 6, 'a+2at': 1} => {'A':14, 'A+2AT':1}
         '''
         dict_all[item.upper()] += 1
@@ -72,9 +94,16 @@ def parse_query(chrom,pos,ref,query):
                 alt = 'ref-homo'
                 var = "%s\t%s\t%s\t%s" % (chrom,pos,ref,alt)
                 dict_alt[var] = 0
-            elif k != ref:
+            elif k != ref and k in BASE:
                 # alt-homo
                 alt = k
+                var = "%s\t%s\t%s\t%s" % (chrom,pos,ref,alt)
+                dict_alt[var] = v
+            elif '+' in k or '-' in k:
+                # Ins or Del
+                indel_info = parse_indel(k)
+                ref = indel_info[0]
+                alt = indel_info[1]
                 var = "%s\t%s\t%s\t%s" % (chrom,pos,ref,alt)
                 dict_alt[var] = v
             else:
@@ -94,44 +123,33 @@ def parse_query(chrom,pos,ref,query):
                         alt = k.upper()
                         var = "%s\t%s\t%s\t%s" % (chrom,pos,ref,alt)
                         dict_alt[var] += dict_all[k]
-                elif k[1] == '+':
-                    # Ins
+                elif '+' in k or '-' in k:
+                    # Ins/Del
                     # 'C+4TACT' => VCF record is: chr pos C CTACT
-                    # get alt allele
-                    alt_allele = []
-                    for i in k:
-                        if i in BASE:
-                            alt_allele.append(i.upper())
-
-                    alt = "".join(alt_allele)
-                    var = "%s\t%s\t%s\t%s" % (chrom,pos,ref,alt)
-                    dict_alt[var] += dict_all[k]
-                elif k[1] == '-':
-                    # Del
                     # 'G-1C' => VCF record is: chr pos GC G
-                    alt_allele = []
-                    for i in k:
-                        if i in BASE:
-                            alt_allele.append(i.upper())
-
-                    del_ref = "".join(alt_allele) # GC
-                    del_alt = del_ref[0]          # G
-                    var = "%s\t%s\t%s\t%s" % (chrom,pos,del_ref,del_alt)
+                    indel_info = parse_indel(k)
+                    ref = indel_info[0]
+                    alt = indel_info[1]
+                    var = "%s\t%s\t%s\t%s" % (chrom,pos,ref,alt)
                     dict_alt[var] += dict_all[k]
                 else:
                     pass
 
-    #print(dict_alt)
-
     return(dict_alt)
 
-def cal_depth(query):
+def depth(query):
     depth = 0
-    for item in query:
-        if item != "*":
+    for i in query:
+        if i != '*':
             depth += 1
-
     return(depth)
+
+def del_depth(query):
+    del_depth = 0
+    for i in query:
+        if i == '*':
+            del_depth += 1
+    return(del_depth)
 
 def main():
     options = parse_args()
@@ -162,36 +180,39 @@ def main():
             '''
             Note: 'all' reads which overlap the region are returned. The first base returned will be the first base of the first read ‘not’ necessarily the first base of the region used in the query. you need to specifiy the 'truncate' param
             '''
-            # max depth = 8000X, mapQ = 30, baseQ = 10
             querybase = pileupcol.get_query_sequences(mark_matches=False,mark_ends=False,add_indels=True)
+
+            # detail depth info
+            my_dict = {}
+            for i in querybase:
+                if i not in my_dict:
+                    my_dict[i] = 1
+                else:
+                    my_dict[i] += 1
 
             # cal depth
             col = pileupcol.reference_pos + 1 # 1-based
             t_chr = t.split(":")[0]
             reg = t_chr + ':' + str(col) + '-' + str(col) # chr1:2-2
             ref_base = pysam_fa.fetch(region=reg).upper() # ref base
-            depth = cal_depth(querybase)
+            real_depth = depth(querybase)
+            #print(t_chr,col,ref_base,my_dict)
 
-            #print(t_chr,col,ref_base,depth,querybase)
+            total_base = len(querybase)
+            del_base = del_depth(querybase)
+            if total_base == del_base:
+                pass
+
+            #print(t_chr,col,ref_base,my_dict)
+            #print(t_chr,col,ref_base,real_depth,querybase)
             
-            '''
-            query_dict = {}
-            for b in querybase:
-                if b not in query_dict:
-                    query_dict[b] = 1;
-                else:
-                    query_dict[b] += 1;
-            print(query_dict)
-            '''
-
             alt_dict = parse_query(t_chr,col,ref_base,querybase)
-            #depth = cal_depth(querybase)
 
-            # output all alt allele
             for k in alt_dict:
-                alt_n = alt_dict[k]
-                freq = round(alt_n/depth,3)
-                v = "%s\t%s\t%s\t%s" % (k,alt_n,depth,freq)
+                #print(k)
+                alt_n = alt_dict[k] # alt reads number
+                freq = round(alt_n/real_depth,3)
+                v = "%s\t%s\t%s\t%s" % (k,alt_n,real_depth,freq)
                 of.write(v+'\n')
     of.close()
 
